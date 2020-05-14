6304d0a7180ec50085c3d61eb85b6b7c2853d9c3
Cailie Skelton <cskelton707@gmail.com>
2020-05-14 20:45:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
   attr_accessor :name, :age, :location, :user_name
+  def initialize(user_name:, name:, age:, location:)
+    @user_name = user_name
+    @name = name
+    @location = location
+    @age = age
+  end
 end
